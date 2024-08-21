51c80af6d626c52fc84d4cf0b2fd656865753b5b
Xich Top <xichtop99@gmail.com>
2024-08-21 23:58:44 +0700

    chore: 📦️ custom swiper

--- src/app/modules/store/store.component.ts
+++ src/app/modules/store/store.component.ts
@@ -1,21 +1,37 @@
 import { C } from "@angular/cdk/keycodes";
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { Component, computed, CUSTOM_ELEMENTS_SCHEMA, signal } from "@angular/core";
 import { AdvertiseComponent } from "@shared/components/advertise/advertise.component";
 import { BannerComponent } from "@shared/components/banner/banner.component";
 import { CategoriesComponent } from "../../shared/components/categories/categories.component";
 import { PromotionComponent } from "@shared/components/promotion/promotion.component";
+import { SvgIcon } from "@libs/svg-icon";
+import { CommonModule } from "@angular/common";
 
 @Component({
   standalone: true,
   selector: "app-store",
   templateUrl: "./store.component.html",
+  styleUrls: ["./store.component.scss"],
   imports: [
     BannerComponent, AdvertiseComponent,
-    CategoriesComponent, PromotionComponent
+    CategoriesComponent, PromotionComponent,
+    SvgIcon, CommonModule
   ],
   host: { class: "w-full" },
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
 })
-export class StoreComponent {}
+export class StoreComponent {
+
+  translateX = signal(0);
+  translateXByPx = computed(() => '[' + this.translateX() + 'rem]');
+
+  scrollLeft() {
+    this.translateX.update(x => x - 4);
+  }
+
+  scrollRight() {
+    this.translateX.update(x => x + 4);
+  }
+}
